d0a75472a7b79e23a411374b553f155db4411835
raviwan694259019 <694259019@webmail.npru.ac.th>
2026-06-25 16:39:52 +0700

    NameAge

---
+++ week2/NameAge.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.print("Enter your name : ");
+    String var1 = IO.readln();
+    IO.print("Enter your ages : ");
+    int var2 = Integer.parseInt(IO.readln());
+    IO.println("Hello, " + var1 + " ! Your age is " + var2);
+}
