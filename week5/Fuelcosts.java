7b9082622cad5ea74336527a8969345e653b8752
Raviwan694259019 <694259019@webmail.npru.ac.th>
2026-07-09 11:53:15 +0700

    add fuelcosts.java in week5

---
+++ week5/Fuelcosts.java
@@ -0,0 +1,12 @@
+void main() {
+    IO.print("Enter distance (km) : ");
+    int distance = Integer.parseInt(IO.readln());
+    IO.print("Enter fuel efficiency (km/liter) : ");
+    int fuel_efficiency = Integer.parseInt(IO.readln());
+    IO.print("Enter fuel prices per liter : ");
+    int fuel_prices = Integer.parseInt(IO.readln());
+    int fuel_used = distance / fuel_efficiency;
+    int total_cost = fuel_used * fuel_prices;
+    IO.println("Fuel used = " + fuel_used + " liters ");
+    IO.println("Total cost = " + total_cost + " baht ");
+}
